8a6708b70d764455f04bead435d68080c2bc9db2
annikaferguson <fergusak@mail.uc.edu>
2023-06-09 15:17:46 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+
+        System.out.println("Please enter your age");
+        age = in.nextInt();
+
+        if(age >= 21)
+        {
+            System.out.println("You get a wristband! Please drink responsibly.");
+        }
     }
 }
